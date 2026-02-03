f21c2d8fbc569133c280c1541325cfc0655a95b1
Ange Koumba <angekoumba754@gmail.com>
2026-02-03 17:31:02 +0200

    usercontext checking

---
+++ alx-react-app-props/src/UserContext.js
@@ -0,0 +1,5 @@
+// src/UserContext.js
+import { createContext } from 'react';
+
+const UserContext = createContext(); // Création du contexte
+export default UserContext;
--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,6 +1,6 @@
 // src/components/UserProfile.jsx
 import { useContext } from 'react';
-import UserContext from '../../../../UserContext';
+import UserContext from '../UserContext';
 
 function UserProfile() {
   const userData = useContext(UserContext);
