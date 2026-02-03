d2efad345e318c5621b792091e20be1c9df963e3
Ange Koumba <angekoumba754@gmail.com>
2026-02-03 16:06:04 +0200

    done work

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,6 @@
 // src/App.jsx
 import React from 'react';
 import WelcomeMessage from './components/WelcomeMessage';
-import UserProfile from './components/UserProfile';
 import Header from './Header';
 import MainContent from './MainContent';
 import Footer from './Footer';
