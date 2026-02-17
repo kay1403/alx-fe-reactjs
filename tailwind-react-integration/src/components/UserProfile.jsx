fb3718f0c3b25d7ccea1456d472b535bc722dc7e
Ange Koumba <angekoumba754@gmail.com>
2026-02-17 17:02:46 +0200

    fixing padding error in userprofile.jsx

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,41 +1,38 @@
 function UserProfile() {
   return (
     <div className="
-      bg-gray-100 
-      p-4 md:p-8
-      max-w-xs md:max-w-sm 
-      mx-auto my-20 
-      rounded-lg 
+      bg-gray-100
+      sm:p-4 md:p-8
+      max-w-xs md:max-w-sm
+      mx-auto my-20
+      rounded-lg
       shadow-lg
-      hover:shadow-xl
-      transition-shadow duration-300
     ">
       <img
         src="https://via.placeholder.com/150"
         alt="User"
         className="
-          rounded-full 
-          w-24 h-24 md:w-36 md:h-36 
-          mx-auto 
-          hover:scale-110 
-          transition-transform duration-300 ease-in-out
+          rounded-full
+          sm:w-24 sm:h-24
+          md:w-36 md:h-36
+          mx-auto
         "
       />
 
       <h1 className="
-        text-lg md:text-xl 
-        text-blue-800 
-        my-4 
-        text-center 
-        hover:text-blue-500 
-        transition-colors duration-300
+        sm:text-lg
+        md:text-xl
+        text-blue-800
+        my-4
+        text-center
       ">
         John Doe
       </h1>
 
       <p className="
-        text-gray-600 
-        text-sm md:text-base 
+        text-gray-600
+        sm:text-sm
+        md:text-base
         text-center
       ">
         Developer at Example Co. Loves to write code and explore new technologies.
