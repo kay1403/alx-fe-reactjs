78b14b85cdd4e8d875ba9b83107c1d1aa3237e08
Ange Koumba <angekoumba754@gmail.com>
2026-02-18 17:12:40 +0200

    form error fixing2

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -5,14 +5,9 @@ import * as Yup from "yup";
 function FormikForm() {
 
   const validationSchema = Yup.object({
-    username: Yup.string()
-      .required("Username is required"),
-    email: Yup.string()
-      .email("Invalid email")
-      .required("Email is required"),
-    password: Yup.string()
-      .min(6, "Password must be at least 6 characters")
-      .required("Password is required")
+    username: Yup.string().required("Username is required"),
+    email: Yup.string().email("Invalid email").required("Email is required"),
+    password: Yup.string().min(6, "Password must be at least 6 characters").required("Password is required")
   });
 
   return (
