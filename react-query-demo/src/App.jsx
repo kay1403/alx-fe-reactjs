68e1f0c55f554d317299ea7c3105e2bb83d93361
Ange Koumba <angekoumba754@gmail.com>
2026-02-18 17:30:55 +0200

    error fixed

--- react-query-demo/src/App.jsx
+++ react-query-demo/src/App.jsx
@@ -1,4 +1,4 @@
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
 import PostsComponent from "./components/PostsComponent";
 
 const queryClient = new QueryClient();
